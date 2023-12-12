81edd5212e3aa69617e8583be976e385802aa6a0
Migue <mtrascasas@gmail.com>
2023-12-12 16:33:04 +0100

    Ejercicio 6 ✅

---
+++ GesTienda/MasterPageAdm.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GesTienda
+{
+    public partial class MasterPageAdm : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnCerrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
